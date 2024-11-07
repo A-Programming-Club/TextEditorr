d5f9ca5136751b09e02fc91bf9c97c868084796f
MayuFujiwara <100046095@csisd.org>
2024-11-07 22:58:27 +0000

    StringTokenizer made.

---
+++ userInput.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class userInput {
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        String userInput = 
+    }
+}
