52fb22e7fcd7e446066a81efacaee302f86557da
Irvinwop <74438277+Irvinwop@users.noreply.github.com>
2024-11-08 01:00:22 -0600

    set up gitignore and basic testing functionality

---
+++ src/test/java/Tests.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.programmingclub.Main;
+
+
+public class Tests {
+    @Test
+    public void testMethod() {
+        assertEquals("test", "test");
+    }
+}
