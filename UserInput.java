c3955eee3ed77000b9ae689a16935d0717fe71f2
MayuFujiwara <100046095@csisd.org>
2024-11-21 22:46:23 +0000

    shd

--- UserInput.java
+++ UserInput.java
@@ -1,17 +1,26 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.PrintWriter;
 import java.util.StringTokenizer;
 
 public class UserInput {
-    public static void main(String[] args) throws IOException{
+    public static File fileInput() throws IOException{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter("temp");
+        //PrintWriter out = new PrintWriter("temp");
         StringTokenizer st = new StringTokenizer(bf.readLine());
         
+
         System.out.print("File name: ");
-        String file = bf.nextToken();
-        String userInput = input.nextToken();
+        String file = bf.readLine();
+
+        while(true){
+            String userInput = bf.readLine();
+            write(file, userInput, true);
+
+        }
+
     }
+    public static void main(){}
 }
